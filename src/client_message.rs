a4c2411089c55adb25067b20d61a63e8c6545c5c
oskarbraten <oskarbraten@users.noreply.github.com>
2021-02-03 20:53:11 +0100

    Update parcel.

--- src/client_message.rs
+++ src/client_message.rs
@@ -2,24 +2,18 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Clone, Serialize, Deserialize, PartialEq, Debug)]
 #[cfg_attr(feature = "json-schema", derive(schemars::JsonSchema))]
-pub enum ChatTarget {
+pub enum ChatReceiverType {
     Direct,
     Group
 }
 
 #[derive(Clone, Serialize, Deserialize, PartialEq, Debug)]
 #[cfg_attr(feature = "json-schema", derive(schemars::JsonSchema))]
-pub enum Contents {
+#[serde(tag = "type")]
+pub enum ClientMessage {
     Chat {
-        target: ChatTarget,
+        target: ChatReceiverType,
         receiver: String,
         text: String
     }
-}
-
-#[derive(Clone, Serialize, Deserialize, PartialEq, Debug)]
-#[cfg_attr(feature = "json-schema", derive(schemars::JsonSchema))]
-pub struct ClientMessage {
-    pub timestamp: u64,
-    pub contents: Contents
 }
--- src/server_message.rs
+++ src/server_message.rs
@@ -2,17 +2,11 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Clone, Serialize, Deserialize, PartialEq, Debug)]
 #[cfg_attr(feature = "json-schema", derive(schemars::JsonSchema))]
-pub enum Contents {
+#[serde(tag = "type")]
+pub enum ServerMessage {
     Chat {
         sender: String,
         text: String
     },
     Empty
-}
-
-#[derive(Clone, Serialize, Deserialize, PartialEq, Debug)]
-#[cfg_attr(feature = "json-schema", derive(schemars::JsonSchema))]
-pub struct ServerMessage {
-    pub timestamp: u64,
-    pub contents: Contents
 }
