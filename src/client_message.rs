96745f91127b539eb1f0b256ebccbe3e095c772f
oskarbraten <oskarbraten@users.noreply.github.com>
2021-02-04 19:46:44 +0100

    Add enum tag type

--- src/client_message.rs
+++ src/client_message.rs
@@ -2,6 +2,7 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Clone, Serialize, Deserialize, PartialEq, Debug)]
 #[cfg_attr(feature = "json-schema", derive(schemars::JsonSchema))]
+#[serde(tag = "type")]
 pub enum ChatReceiver {
     All,
     Direct {
